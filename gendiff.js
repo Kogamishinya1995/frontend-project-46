5678674000384159d75f9c9555dbf3ca4da42936
aleksandr <79081103268@yandex.ru>
2024-03-13 01:41:44 +0600

    1 stage///

--- gendiff.js
+++ gendiff.js
@@ -1,14 +1,10 @@
 #!/usr/bin/env node
 
-import program from 'commander';
+import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .option('-h, --help', 'output usage information')
-  .parse(process.argv);
+  .option('-V, --version', 'output the version number');
 
-if (program.help) {
-  console.log('Usage: gendiff [options]');
-  console.log('');
-  console.log('Compares two configuration files and shows a difference.');
-}
+program.parse();
