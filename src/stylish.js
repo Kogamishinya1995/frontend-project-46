f450993775c121ae470cd3efc1afae28139f058e
aleksandr <79081103268@yandex.ru>
2024-05-01 10:59:50 +0600

    7 step 90 percent

--- src/stylish.js
+++ src/stylish.js
@@ -1,27 +1,30 @@
 import _ from 'lodash';
 
-const formatDiff = (diff, depth = 1) => { 
-    if (!_.isObject(diff)) { 
-      return `${diff}`; 
-    } 
-   
-    const indentSize = 4; 
-    const indent = ' '.repeat(indentSize * depth); 
-    const bracketIndent = ' '.repeat(indentSize * (depth - 1)); 
-   
-    const lines = Object.entries(diff).flatMap(([key, value]) => { 
-      if (_.isObject(value)) { 
-        return [ 
-          `${indent}  ${key}: {`, 
-          `${formatDiff(value, depth + 1)}`, 
-          `${bracketIndent}  }`, 
-        ]; 
-      } 
-   
-      return `${indent}  ${key}: ${value}`; 
-    }); 
-   
-    return `{\n${lines.join('\n')}\n${bracketIndent}}`; 
-  }; 
-   
-  export default formatDiff; 
+  const formatDiff = (diff, depth = 1) => {
+    if (!_.isObject(diff)) {
+        return `${diff}`;
+    }
+
+    const indentSize = 2; // количество отступов для каждого уровня вложенности
+    const indentChar = '.'; // символ для формирования отступов
+    const specialChar = '+'; // специальный символ
+    const indent = `${indentChar.repeat(indentSize * depth)}`; // формирование отступа
+    const bracketIndent = `${indentChar.repeat(indentSize * (depth - 1))}`; // отступ для закрывающей скобки
+
+    const lines = Object.entries(diff).flatMap(([key, value]) => {
+        if (_.isObject(value)) {
+            return [
+                `${indent}${key}: {`,
+                `${formatDiff(value, depth + 1)}`,
+                `${bracketIndent}}`,
+            ];
+        }
+
+        const special = depth > 1 ? `${specialChar} ` : ''; // проверка на специальный символ
+        return `${indent}${special}${key}: ${value}`;
+    });
+
+    return `{\n${lines.join('\n')}\n${bracketIndent}}`;
+};
+
+export default formatDiff; 
