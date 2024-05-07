d100d8eaada7c108170f151532e93a5f5a9e8143
aleksandr <79081103268@yandex.ru>
2024-05-07 21:55:41 +0600

    10 step 5

--- formaters/stylish.js
+++ formaters/stylish.js
@@ -4,7 +4,7 @@ const formatValue = (value, depth) => {
   if (_.isObject(value)) {
     const keys = Object.keys(value).sort();
     const indentSize = 4;
-    const indentStr = ' '.repeat(indentSize * depth);
+    const indentStr = ''.repeat(indentSize * depth);
     const formattedEntries = keys.map((key) => `${indentStr}  ${key}: ${formatValue(value[key], depth + 1)}`);
     return `{\n${formattedEntries.join('\n')}\n${indentStr}}`;
   }
