9aac70780b28da7a8127eadd5576a8e5f986864b
aleksandr <79081103268@yandex.ru>
2024-05-08 13:25:10 +0600

    10 step 17

--- formaters/plain.js
+++ formaters/plain.js
@@ -10,15 +10,14 @@ const formatValue = (value) => {
 const formatDiff = (diff, path = '') => {
   const entries = Object.entries(diff).flatMap(([key, {
     status, value, value1, value2, children,
-  },
-  ]) => {
+  }]) => {
     const currentPath = path ? `${path}.${key}` : key;
 
     switch (status) {
       case 'added':
         return `Property '${currentPath}' was added with value: ${formatValue(value)}`;
       case 'removed':
-        return `Property '${currentPath}' was removed`;
+        return `Property '${currentPath}' was removed with value: ${formatValue(value)}`;
       case 'updated':
         return `Property '${currentPath}' was updated. From ${formatValue(value1)} to ${formatValue(value2)}`;
       case 'nested':
