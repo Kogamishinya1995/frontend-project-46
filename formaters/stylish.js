849c11d5e6c4242d050b8e8dc9321ebc888cf0b7
aleksandr <79081103268@yandex.ru>
2024-05-08 12:16:45 +0600

    10 step 7

--- formaters/stylish.js
+++ formaters/stylish.js
@@ -34,7 +34,7 @@ const formatDiff = (diff, depth = 1) => {
         throw new Error(`Unknown status: ${status}`);
     }
   }).flat();
-  return `{\n${formattedEntries.join('\n')}\n}`;
+  return `{\n${formattedEntries.join('\n')}\n${indentStr}}`;
 };
 
 export default formatDiff;
