e26cb00046f432d4e993ed9e3265e9e1f2f612bf
aleksandr <79081103268@yandex.ru>
2024-05-08 13:34:26 +0600

    10 step 19

--- formaters/plain.js
+++ formaters/plain.js
@@ -8,20 +8,18 @@ const formatValue = (value) => {
 };
 
 const formatDiff = (diff, path = '') => {
-  const entries = Object.entries(diff).flatMap(([key, {
-    status, value, value1, value2, children,
-  }]) => {
+  const entries = Object.entries(diff).flatMap(([key, { status, value, value1, value2, children }]) => {
     const currentPath = path ? `${path}.${key}` : key;
 
     switch (status) {
       case 'added':
-        return `Property '${currentPath}' was added with value: ${formatValue(value)}`;
+        return `- Property '${currentPath}' was added with value: ${formatValue(value)}`;
       case 'removed':
-        return `Property '${currentPath}' was removed with value: ${formatValue(value)}`;
+        return `- Property '${currentPath}' was removed`;
       case 'updated':
-        return `Property '${currentPath}' was updated. From ${formatValue(value1)} to ${formatValue(value2)}`;
+        return `- Property '${currentPath}' was updated. From ${formatValue(value1)} to ${formatValue(value2)}`;
       case 'nested':
-        return formatDiff(children, currentPath);
+        return formatDiff(children, currentPath).map(entry => `  ${entry}`);
       case 'unchanged':
         return [];
       default:
@@ -29,7 +27,10 @@ const formatDiff = (diff, path = '') => {
     }
   });
 
-  return entries.join('\n');
+  return [
+    ...entries.filter(entry => !Array.isArray(entry)).flatMap(entry => entry.split('\n')),
+    ...entries.flatMap(entry => (Array.isArray(entry) ? entry : [])),
+  ].join('\n');
 };
 
 export default formatDiff;
