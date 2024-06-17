a38f648d2a97d647c46954859a987d5ec264eda0
aleksandr <79081103268@yandex.ru>
2024-06-17 21:12:52 +0600

    10 step linter fix 1

--- __tests__/diffGenerator.test.js
+++ __tests__/diffGenerator.test.js
@@ -1,26 +1,20 @@
-import generateDiff from '../src/diffGenerator';
+import generateDiff from '../src/diffGenerator'; 
 
-test('test generateDiff', () => {
-  const data1 = {
-    key1: 'value1',
-    key2: 123,
-    key3: { nestedKey: 'nestedValue' },
-  };
-
-  const data2 = {
-    key1: 'value1',
-    key2: 456,
-    key4: 'value4',
-  };
-
-  const diff = generateDiff(data1, data2);
-
-  const expectedDiff = {
-    key1: { status: 'unchanged', value: 'value1' },
-    key2: { status: 'updated', value1: 123, value2: 456 },
-    key3: { status: 'removed', value: { nestedKey: 'nestedValue' } },
-    key4: { status: 'added', value: 'value4' },
-  };
-
+test('generateDiff - should identify updated, removed and added values', () => {  
+  const data1 = { 
+    key1: 'value1',    key2: 123, 
+    key3: { nestedKey: 'nestedValue' },  
+  }; 
+  const data2 = { 
+    key1: 'value1',    key2: 456, 
+    key4: 'value4',  
+  }; 
+  const diff = generateDiff(data1, data2); 
+  const expectedDiff = { 
+    key1: { status: 'unchanged', value: 'value1' },    
+    key2: { status: 'updated', value1: 123, value2: 456 }, 
+    key3: { status: 'removed', value: { nestedKey: 'nestedValue' } },    
+    key4: { status: 'added', value: 'value4' }, 
+  }; 
   expect(diff).toEqual(expectedDiff);
-});
+}); 
