52ec0573d8a2fc34293cded29a87ee3675d7361b
aleksandr <79081103268@yandex.ru>
2024-06-17 19:36:14 +0600

    10 step plaine.js already fix 2

--- formaters/plain.js
+++ formaters/plain.js
@@ -8,25 +8,29 @@ const formatValue = (value) => {
 };
 
 const formatDiff = (diff, path = '') => {
-  return Object.entries(diff).flatMap(([key, { status, value, value1, value2, children }]) => {
+  const entries = Object.entries(diff).flatMap(([key, {
+    status, value, value1, value2, children,
+  }]) => {
     const currentPath = path ? `${path}.${key}` : key;
 
     switch (status) {
       case 'added':
-        return `Property '${currentPath}' was added with value: ${formatValue(value)}`;
+        return [`Property '${currentPath}' was added with value: ${formatValue(value)}`];
       case 'removed':
-        return `Property '${currentPath}' was removed with value: ${formatValue(value)}`;
+        return [`Property '${currentPath}' was removed`];
       case 'updated':
-        return `Property '${currentPath}' was updated. From ${formatValue(value1)} to ${formatValue(value2)}`;
+        return [`Property '${currentPath}' was updated. From ${formatValue(value1)} to ${formatValue(value2)}`];
       case 'nested':
         const nestedDiff = formatDiff(children, currentPath);
-        return nestedDiff ? `Property '${currentPath}' was updated. From ${nestedDiff.split('\n').join('\n')}` : [];
+        return nestedDiff.length > 0 ? nestedDiff : [];
       case 'unchanged':
         return [];
       default:
         throw new Error(`Unknown status: ${status}`);
     }
-  }).join('\n');
+  });
+
+  return entries.join('\n');
 };
 
 export default formatDiff;
