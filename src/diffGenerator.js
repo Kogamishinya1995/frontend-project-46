e3600d37b8a02f0d1a261185a488f6d5b46c6141
aleksandr <79081103268@yandex.ru>
2024-06-17 23:23:53 +0600

    10 step linter fix 6

--- src/diffGenerator.js
+++ src/diffGenerator.js
@@ -2,13 +2,13 @@ import _ from 'lodash';
 
 const generateDiff = (data1, data2) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
-  const diff = keys.reduce((acc, key) => {
+  return keys.reduce((acc, key) => {
     if (!_.has(data1, key)) {
       acc[key] = { status: 'added', value: data2[key] };
     } else if (!_.has(data2, key)) {
       acc[key] = { status: 'removed', value: data1[key] };
     } else if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      acc[key] = { status: 'nested', children: generateDiff(data1[key], data2[key]) };
+      acc[key] = { status: 'nested', children: generateDiff(_.cloneDeep(data1[key]), _.cloneDeep(data2[key])) };
     } else if (!_.isEqual(data1[key], data2[key])) {
       acc[key] = { status: 'updated', value1: data1[key], value2: data2[key] };
     } else {
@@ -16,7 +16,6 @@ const generateDiff = (data1, data2) => {
     }
     return acc;
   }, {});
-  return diff;
 };
 
 export default generateDiff;
