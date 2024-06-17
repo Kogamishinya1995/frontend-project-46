2285d2e20c3cdc3e3ccd9a20e764612f125b61bd
aleksandr <79081103268@yandex.ru>
2024-06-17 19:47:16 +0600

    10 step plaine.js already fix 5

--- formaters/plain.js
+++ formaters/plain.js
@@ -8,31 +8,27 @@ const formatValue = (value) => {
 };
 
 const formatDiff = (diff, path = '') => {
-  const entries = Object.entries(diff)
-    .sort(([keyA], [keyB]) => keyA.localeCompare(keyB))
-    .flatMap(([key, { status, value, value1, value2, children }]) => {
-      const currentPath = path ? `${path}.${key}` : key;
+  const entries = Object.entries(diff).flatMap(([key, {
+    status, value, value1, value2, children,
+  }]) => {
+    const currentPath = path ? `${path}.${key}` : key;
 
-      switch (status) {
-        case 'added':
-          return [`Property '${currentPath}' was added with value: ${formatValue(value)}`];
-        case 'removed':
-          return [`Property '${currentPath}' was removed`];
-        case 'updated':
-          if (_.isObject(value1) && _.isObject(value2)) {
-            const nestedDiff = formatDiff(children, currentPath);
-            return nestedDiff.length > 0 ? nestedDiff : [];
-          }
-          return [`Property '${currentPath}' was updated. From ${formatValue(value1)} to ${formatValue(value2)}`];
-        case 'nested':
-          const nestedDiff = formatDiff(children, currentPath);
-          return nestedDiff.length > 0 ? nestedDiff : [];
-        case 'unchanged':
-          return [];
-        default:
-          throw new Error(`Unknown status: ${status}`);
-      }
-    });
+    switch (status) {
+      case 'added':
+        return [`Property '${currentPath}' was added with value: ${formatValue(value)}`];
+      case 'removed':
+        return [`Property '${currentPath}' was removed`];
+      case 'updated':
+        return [`Property '${currentPath}' was updated. From ${formatValue(value1)} to ${formatValue(value2)}`];
+      case 'nested':
+        const nestedDiff = formatDiff(children, currentPath);
+        return nestedDiff.length > 0 ? nestedDiff : [];
+      case 'unchanged':
+        return [];
+      default:
+        throw new Error(`Unknown status: ${status}`);
+    }
+  });
 
   return entries.join('\n');
 };
