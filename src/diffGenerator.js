adc220b4b4c6ae66148788fc4f179927978b3be1
aleksandr <79081103268@yandex.ru>
2024-06-17 23:31:21 +0600

    10 step linter fix 7

--- src/diffGenerator.js
+++ src/diffGenerator.js
@@ -2,20 +2,23 @@ import _ from 'lodash';
 
 const generateDiff = (data1, data2) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
-  return keys.reduce((acc, key) => {
+  const diff = keys.reduce((acc, key) => {
+    // Создаем новый объект для acc вместо его изменения
+    const newAcc = { ...acc };
     if (!_.has(data1, key)) {
-      acc[key] = { status: 'added', value: data2[key] };
+      newAcc[key] = { status: 'added', value: data2[key] };
     } else if (!_.has(data2, key)) {
-      acc[key] = { status: 'removed', value: data1[key] };
+      newAcc[key] = { status: 'removed', value: data1[key] };
     } else if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      acc[key] = { status: 'nested', children: generateDiff(_.cloneDeep(data1[key]), _.cloneDeep(data2[key])) };
+      newAcc[key] = { status: 'nested', children: generateDiff(data1[key], data2[key]) };
     } else if (!_.isEqual(data1[key], data2[key])) {
-      acc[key] = { status: 'updated', value1: data1[key], value2: data2[key] };
+      newAcc[key] = { status: 'updated', value1: data1[key], value2: data2[key] };
     } else {
-      acc[key] = { status: 'unchanged', value: data1[key] };
+      newAcc[key] = { status: 'unchanged', value: data1[key] };
     }
-    return acc;
+    return newAcc;
   }, {});
+  return diff;
 };
 
 export default generateDiff;
