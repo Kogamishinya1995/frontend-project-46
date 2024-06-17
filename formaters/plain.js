8457e34c3a5ca6d8d1200ddc724b5d96234c926e
aleksandr <79081103268@yandex.ru>
2024-06-17 20:03:36 +0600

    10 step plaine.js already fix 9

--- formaters/plain.js
+++ formaters/plain.js
@@ -4,6 +4,9 @@ const formatValue = (value) => {
   if (_.isObject(value)) {
     return '[complex value]';
   }
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
   return value;
 };
 
