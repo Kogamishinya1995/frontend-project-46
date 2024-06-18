6e1cc92baa1971d99d4ffeeccdc150283e19a8c9
aleksandr <79081103268@yandex.ru>
2024-06-18 13:05:58 +0600

    10 step linter fix 10

--- src/diffGenerator.js
+++ src/diffGenerator.js
@@ -3,21 +3,18 @@ import _ from 'lodash';
 const generateDiff = (data1, data2) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
   const diff = keys.reduce((acc, key) => {
-    let inner = { ...acc };
-
     if (!_.has(data1, key)) {
-      inner[key] = { status: 'added', value: data2[key] };
+      acc[key] = { status: 'added', value: data2[key] };
     } else if (!_.has(data2, key)) {
-      inner[key] = { status: 'removed', value: data1[key] };
+      acc[key] = { status: 'removed', value: data1[key] };
     } else if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      inner[key] = { status: 'nested', children: generateDiff(data1[key], data2[key]) };
+      acc[key] = { status: 'nested', children: generateDiff(data1[key], data2[key]) };
     } else if (!_.isEqual(data1[key], data2[key])) {
-      inner[key] = { status: 'updated', value1: data1[key], value2: data2[key] };
+      acc[key] = { status: 'updated', value1: data1[key], value2: data2[key] };
     } else {
-      inner[key] = { status: 'unchanged', value: data1[key] };
+      acc[key] = { status: 'unchanged', value: data1[key] };
     }
-
-    return inner;
+    return acc;
   }, {});
   return diff;
 };
