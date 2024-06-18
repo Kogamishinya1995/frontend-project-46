83d5e7f75a70b748ad4cf9aadf14b73e0cd28d47
aleksandr <79081103268@yandex.ru>
2024-06-18 17:08:40 +0600

    10 step linter fix 17

--- formaters/stylish.js
+++ formaters/stylish.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 const formatValue = (value, depth) => {
   if (_.isObject(value)) {
-    const keys = Object.keys(value).sort();
+    const keys = _.sortBy(Object.keys(value));
     const indentSize = 4;
     const indentStr = ' '.repeat(indentSize * depth - 2);
     const formattedEntries = keys.map((key) => `${indentStr}  ${key}: ${formatValue(value[key], depth + 1)}`);
@@ -12,13 +12,11 @@ const formatValue = (value, depth) => {
 };
 
 const formatDiff = (diff, depth = 1) => {
-  const keys = Object.keys(diff).sort();
+  const keys = _.sortBy(Object.keys(diff));
   const indentSize = 4;
   const indentStr = ' '.repeat(indentSize * depth - 2);
   const formattedEntries = keys.map((key) => {
-    const {
-      status, value, value1, value2, children,
-    } = diff[key];
+    const { status, value, value1, value2, children } = diff[key];
     switch (status) {
       case 'added':
         return `${indentStr}+ ${key}: ${formatValue(value, depth + 1)}`;
