0ce9c1ffcd70446501f0235ca93a00910e6df4b2
aleksandr <79081103268@yandex.ru>
2024-06-19 23:01:36 +0600

    test fixed 6

--- src/diffGenerator.js
+++ src/diffGenerator.js
@@ -1,19 +1,29 @@
 import _ from 'lodash';
 
+const makeNode = (status, value1 = null, value2 = null, children = null) => ({
+  status,
+  value1,
+  value2,
+  children,
+});
+
 const calculateDiff = (data1, data2) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
 
   return keys.reduce((acc, key) => {
     if (!_.has(data1, key)) {
-      return { ...acc, [key]: { status: 'added', value: data2[key] } };
-    } if (!_.has(data2, key)) {
-      return { ...acc, [key]: { status: 'removed', value: data1[key] } };
-    } if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      return { ...acc, [key]: { status: 'nested', children: calculateDiff(data1[key], data2[key]) } };
-    } if (!_.isEqual(data1[key], data2[key])) {
-      return { ...acc, [key]: { status: 'updated', value1: data1[key], value2: data2[key] } };
+      return { ...acc, [key]: makeNode('added', null, data2[key]) };
     }
-    return { ...acc, [key]: { status: 'unchanged', value: data1[key] } };
+    if (!_.has(data2, key)) {
+      return { ...acc, [key]: makeNode('removed', data1[key], null) };
+    }
+    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+      return { ...acc, [key]: makeNode('nested', null, null, calculateDiff(data1[key], data2[key])) };
+    }
+    if (!_.isEqual(data1[key], data2[key])) {
+      return { ...acc, [key]: makeNode('updated', data1[key], data2[key]) };
+    }
+    return { ...acc, [key]: makeNode('unchanged', data1[key], null) };
   }, {});
 };
 
