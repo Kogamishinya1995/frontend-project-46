5cef22d0bceee52d3f34d6e7a5d02860272aac92
aleksandr <79081103268@yandex.ru>
2024-07-07 15:59:28 +0600

    new stylish 1

--- formaters/stylish.js
+++ formaters/stylish.js
@@ -5,33 +5,44 @@ const formatValue = (value, depth) => {
     const keys = _.sortBy(Object.keys(value));
     const indentSize = 4;
     const indentStr = ' '.repeat(indentSize * depth - 2);
-    const formattedEntries = keys.map((key) => `${indentStr}  ${key}: ${formatValue(value[key], depth + 1)}`);
-    return `{\n${formattedEntries.join('\n')}\n${indentStr.slice(0, -2)}}`;
+    const formattedEntries = keys.map(
+      (key) => `${indentStr}  ${key}: ${formatValue(value[key], depth + 1)}`,
+    );
+    return `\n${formattedEntries.join('\n')}\n${indentStr.slice(0, -2)}`;
   }
   return value;
 };
 
 const formatDiff = (diff, depth = 1) => {
+  const keys = _.sortBy(Object.keys(diff));
   const indentSize = 4;
   const indentStr = ' '.repeat(indentSize * depth - 2);
-  const formattedEntries = diff.map((node) => {
-    const { type, value, value2, children } = node;
+  const formattedEntries = keys.map((key) => {
+    const {
+      type,
+      value,
+      value2,
+      children,
+    } = diff[key];
     switch (type) {
       case 'added':
-        return `${indentStr}+ ${formatValue(value, depth + 1)}`;
+        return `${indentStr}+ ${key}: ${formatValue(value, depth + 1)}`;
       case 'removed':
-        return `${indentStr}- ${formatValue(value, depth + 1)}`;
+        return `${indentStr}- ${key}: ${formatValue(value, depth + 1)}`;
       case 'updated':
-        return [`${indentStr}- ${formatValue(value, depth + 1)}`, `${indentStr}+ ${formatValue(value2, depth + 1)}`];
+        return [
+          `${indentStr}- ${key}: ${formatValue(value, depth + 1)}`,
+          `${indentStr}+ ${key}: ${formatValue(value2, depth + 1)}`,
+        ];
       case 'nested':
-        return `${indentStr}  ${formatDiff(children, depth + 1)}`;
+        return `${indentStr}  ${key}: ${formatDiff(children, depth + 1)}`;
       case 'unchanged':
-        return `${indentStr}  ${formatValue(value, depth + 1)}`;
+        return `${indentStr}  ${key}: ${formatValue(value, depth + 1)}`;
       default:
-        throw new Error(`Unknown type: ${type}`);
+        throw new Error(`Unknown status: ${type}`);
     }
   }).flat();
-  return `{\n${formattedEntries.join('\n')}\n${indentStr.slice(0, -2)}}`;
+  return `\n${formattedEntries.join('\n')}\n${indentStr.slice(0, -2)}`;
 };
 
 export default formatDiff;
