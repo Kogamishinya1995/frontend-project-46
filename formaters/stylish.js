f60b124eae4212552eb4ccc9699ab6d628c66e0f
aleksandr <79081103268@yandex.ru>
2024-07-07 15:42:16 +0600

    new stylish

--- formaters/stylish.js
+++ formaters/stylish.js
@@ -12,26 +12,23 @@ const formatValue = (value, depth) => {
 };
 
 const formatDiff = (diff, depth = 1) => {
-  const keys = _.sortBy(Object.keys(diff));
   const indentSize = 4;
   const indentStr = ' '.repeat(indentSize * depth - 2);
-  const formattedEntries = keys.map((key) => {
-    const {
-      status, value, value1, value2, children,
-    } = diff[key];
-    switch (status) {
+  const formattedEntries = diff.map((node) => {
+    const { type, value, value2, children } = node;
+    switch (type) {
       case 'added':
-        return `${indentStr}+ ${key}: ${formatValue(value, depth + 1)}`;
+        return `${indentStr}+ ${formatValue(value, depth + 1)}`;
       case 'removed':
-        return `${indentStr}- ${key}: ${formatValue(value, depth + 1)}`;
+        return `${indentStr}- ${formatValue(value, depth + 1)}`;
       case 'updated':
-        return [`${indentStr}- ${key}: ${formatValue(value1, depth + 1)}`, `${indentStr}+ ${key}: ${formatValue(value2, depth + 1)}`];
+        return [`${indentStr}- ${formatValue(value, depth + 1)}`, `${indentStr}+ ${formatValue(value2, depth + 1)}`];
       case 'nested':
-        return `${indentStr}  ${key}: ${formatDiff(children, depth + 1)}`;
+        return `${indentStr}  ${formatDiff(children, depth + 1)}`;
       case 'unchanged':
-        return `${indentStr}  ${key}: ${formatValue(value, depth + 1)}`;
+        return `${indentStr}  ${formatValue(value, depth + 1)}`;
       default:
-        throw new Error(`Unknown status: ${status}`);
+        throw new Error(`Unknown type: ${type}`);
     }
   }).flat();
   return `{\n${formattedEntries.join('\n')}\n${indentStr.slice(0, -2)}}`;
