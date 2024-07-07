98e35ec6831acc0d24b9e220b63a44b67cae9768
aleksandr <79081103268@yandex.ru>
2024-07-07 16:33:31 +0600

    new stylish 3

--- formaters/stylish.js
+++ formaters/stylish.js
@@ -3,22 +3,27 @@ import _ from 'lodash';
 const formatValue = (value, depth) => {
   if (_.isObject(value)) {
     const keys = _.sortBy(Object.keys(value));
-    const indentSize = 2;
-    const indentStr = ' '.repeat(indentSize * depth);
+    const indentSize = 4;
+    const indentStr = ' '.repeat(indentSize * depth - 1);
     const formattedEntries = keys.map(
-      (key) => `${indentStr}  ${key}: ${formatValue(value[key], depth + 1)}`
+      (key) => `${indentStr}  ${key}: ${formatValue(value[key], depth + 1)}`,
     );
-    return `{\n${formattedEntries.join(',\n')}\n${indentStr}}`;
+    return `\n${formattedEntries.join('\n')}\n${indentStr.slice(0, -2)}`;
   }
   return value;
 };
 
 const formatDiff = (diff, depth = 1) => {
   const keys = _.sortBy(Object.keys(diff));
-  const indentSize = 2;
-  const indentStr = ' '.repeat(indentSize * depth);
+  const indentSize = 4;
+  const indentStr = ' '.repeat(indentSize * depth - 2);
   const formattedEntries = keys.map((key) => {
-    const { type, value, value2, children } = diff[key];
+    const {
+      type,
+      value,
+      value2,
+      children,
+    } = diff[key];
     switch (type) {
       case 'added':
         return `${indentStr}+ ${key}: ${formatValue(value, depth + 1)}`;
@@ -37,7 +42,7 @@ const formatDiff = (diff, depth = 1) => {
         throw new Error(`Unknown status: ${type}`);
     }
   }).flat();
-  return `{\n${formattedEntries.join(',\n')}\n${indentStr.slice(0, -2)}}`;
+  return `\n${formattedEntries.join('\n')}\n${indentStr.slice(0, -2)}`;
 };
 
 export default formatDiff;
