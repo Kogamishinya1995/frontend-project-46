983c6dd6264808e5291b576e7e1cb72d4894079c
aleksandr <79081103268@yandex.ru>
2024-07-07 16:44:24 +0600

    new formaters 4

--- formaters/stylish.js
+++ formaters/stylish.js
@@ -1,39 +1,29 @@
 import _ from 'lodash';
-
 const formatValue = (value, depth) => {
   if (_.isObject(value)) {
     const keys = _.sortBy(Object.keys(value));
     const indentSize = 4;
-    const indentStr = ' '.repeat(indentSize * depth - 1);
-    const formattedEntries = keys.map(
-      (key) => `${indentStr}  ${key}: ${formatValue(value[key], depth + 1)}`,
-    );
-    return `\n${formattedEntries.join('\n')}\n${indentStr.slice(0, -2)}`;
+    const indentStr = ' '.repeat(indentSize * depth - 2);
+    const formattedEntries = keys.map((key) => `${indentStr}  ${key}: ${formatValue(value[key], depth + 1)}`);
+    return `{\n${formattedEntries.join('\n')}\n${indentStr.slice(0, -2)}}`;
   }
   return value;
 };
 
 const formatDiff = (diff, depth = 1) => {
-  const keys = _.sortBy(Object.keys(diff));
+  const keys = _.sortBy(diff.map((node) => node.type));
   const indentSize = 4;
   const indentStr = ' '.repeat(indentSize * depth - 2);
   const formattedEntries = keys.map((key) => {
-    const {
-      type,
-      value,
-      value2,
-      children,
-    } = diff[key];
+    const node = diff.find((n) => n.type === key);
+    const { type, value, value2, children } = node;
     switch (type) {
       case 'added':
         return `${indentStr}+ ${key}: ${formatValue(value, depth + 1)}`;
       case 'removed':
         return `${indentStr}- ${key}: ${formatValue(value, depth + 1)}`;
       case 'updated':
-        return [
-          `${indentStr}- ${key}: ${formatValue(value, depth + 1)}`,
-          `${indentStr}+ ${key}: ${formatValue(value2, depth + 1)}`,
-        ];
+        return [`${indentStr}- ${key}: ${formatValue(value, depth + 1)}`, `${indentStr}+ ${key}: ${formatValue(value2, depth + 1)}`];
       case 'nested':
         return `${indentStr}  ${key}: ${formatDiff(children, depth + 1)}`;
       case 'unchanged':
@@ -42,7 +32,7 @@ const formatDiff = (diff, depth = 1) => {
         throw new Error(`Unknown status: ${type}`);
     }
   }).flat();
-  return `\n${formattedEntries.join('\n')}\n${indentStr.slice(0, -2)}`;
+  return `{\n${formattedEntries.join('\n')}\n${indentStr.slice(0, -2)}}`;
 };
 
 export default formatDiff;
