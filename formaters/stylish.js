4135301c2b419b9defee90f496f79a8fcd54738e
aleksandr <79081103268@yandex.ru>
2024-07-07 23:42:30 +0600

    new formaters 7

--- formaters/stylish.js
+++ formaters/stylish.js
@@ -12,28 +12,26 @@ const formatValue = (value, depth) => {
 };
 
 const formatDiff = (diff, depth = 1) => {
-  const keys = _.sortBy(diff.map((node) => node.type));
+  const keys = _.sortBy(Object.keys(diff));
   const indentSize = 4;
   const indentStr = ' '.repeat(indentSize * depth - 2);
   const formattedEntries = keys.map((key) => {
-    const node = diff.find((n) => n.type === key);
-    const { type, value, value2, children } = node;
-    switch (type) {
+    const {
+      status, value, value1, value2, children,
+    } = diff[key];
+    switch (status) {
       case 'added':
         return `${indentStr}+ ${key}: ${formatValue(value, depth + 1)}`;
       case 'removed':
         return `${indentStr}- ${key}: ${formatValue(value, depth + 1)}`;
       case 'updated':
-        return [
-          `${indentStr}- ${key}: ${formatValue(value, depth + 1)}`,
-          `${indentStr}+ ${key}: ${formatValue(value2, depth + 1)}`,
-        ];
+        return [`${indentStr}- ${key}: ${formatValue(value1, depth + 1)}`, `${indentStr}+ ${key}: ${formatValue(value2, depth + 1)}`];
       case 'nested':
         return `${indentStr}  ${key}: ${formatDiff(children, depth + 1)}`;
       case 'unchanged':
         return `${indentStr}  ${key}: ${formatValue(value, depth + 1)}`;
       default:
-        throw new Error(`Unknown status: ${type}`);
+        throw new Error(`Unknown status: ${status}`);
     }
   }).flat();
   return `{\n${formattedEntries.join('\n')}\n${indentStr.slice(0, -2)}}`;
--- src/diffGenerator.js
+++ src/diffGenerator.js
@@ -1,45 +1,20 @@
 import _ from 'lodash';
 
-const makeNode = ({
-  type, value, value2, children,
-}) => {
-  const base = {
-    type,
-  };
-
-  if (value !== undefined) {
-    base.value = value;
-  }
-
-  if (value2 !== undefined) {
-    base.value2 = value2;
-  }
-
-  if (children !== undefined) {
-    base.children = children;
-  }
-
-  return base;
-};
-
 const calculateDiff = (data1, data2) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
 
   return keys.reduce((acc, key) => {
     if (!_.has(data1, key)) {
-      return [...acc, makeNode({ type: 'added', value: data2[key] })];
+      return { ...acc, [key]: { status: 'added', value: data2[key] } };
+    } if (!_.has(data2, key)) {
+      return { ...acc, [key]: { status: 'removed', value: data1[key] } };
+    } if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+      return { ...acc, [key]: { status: 'nested', children: calculateDiff(data1[key], data2[key]) } };
+    } if (!_.isEqual(data1[key], data2[key])) {
+      return { ...acc, [key]: { status: 'updated', value1: data1[key], value2: data2[key] } };
     }
-    if (!_.has(data2, key)) {
-      return [...acc, makeNode({ type: 'removed', value: data1[key] })];
-    }
-    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      return [...acc, makeNode({ type: 'nested', children: calculateDiff(data1[key], data2[key]) })];
-    }
-    if (!_.isEqual(data1[key], data2[key])) {
-      return [...acc, makeNode({ type: 'updated', value: data1[key], value2: data2[key] })];
-    }
-    return [...acc, makeNode({ type: 'unchanged', value: data1[key] })];
-  }, []);
+    return { ...acc, [key]: { status: 'unchanged', value: data1[key] } };
+  }, {});
 };
 
 export default calculateDiff;
